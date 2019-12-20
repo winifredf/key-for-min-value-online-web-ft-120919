773a40d58f1864694bfa0430f5df1ee0ec90518e
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 02:18:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_value = 0
+  lowest_key = nil
   
+  name_hash.each do |key,value|
+    
 end
