0114a478ee48317a648c02b03a6f16692c406798
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 02:20:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,11 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   
   name_hash.each do |key,value|
-    
+    if  lowest_value == 0 || value < 
+      lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
