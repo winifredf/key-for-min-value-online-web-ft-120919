3c884890cb771930f58463e738d87954c77bbb6e
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 02:14:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,13 @@
 def key_for_min_value(name_hash)
   current_minimum_val = nil
   current_key_assoc_w_min_val = nil
-  
+    if  current_minimum_val == nil
+      current_minimum_val = value
+      current_key_assoc_w_min_val = key
+    else
+    if  current_minimum_val > value
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key
+    end
+  end  
 end
